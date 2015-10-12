575ea2c3eef226428e98e4024bb84fcaabfa155b
Henning <henningcorneliussen@gmail.com>
2015-10-12 07:57:41 +0200

    onclick add i activity main anime classe laget i mainactivity

--- app/src/main/java/com/example/bilal/s198520_mappe2/MainActivity.java
+++ app/src/main/java/com/example/bilal/s198520_mappe2/MainActivity.java
@@ -43,4 +43,8 @@ public class MainActivity extends ActionBarActivity {
         buckysInput.setText("");
     }
 
+    public void anime(View view){
+        view.setBackgroundResource(nyttBildeFixerSenere);
+    }
+
 }
