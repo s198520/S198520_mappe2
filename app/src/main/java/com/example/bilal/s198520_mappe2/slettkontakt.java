6777403795a6a22e1d166e7607bcda7c52bb66c6
Bilal Khan <bilal.1410@hotmail.com>
2015-10-14 12:17:28 +0200

    meny done..?

---
+++ app/src/main/java/com/example/bilal/s198520_mappe2/slettkontakt.java
@@ -0,0 +1,26 @@
+package com.example.bilal.s198520_mappe2;
+
+/**
+ * Created by bilal on 10/14/2015.
+ */
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class slettkontakt extends Activity {
+
+    protected void onCreate(Bundle savedInstanceState ){
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.nykontakt);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu){
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+}
